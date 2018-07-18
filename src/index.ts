6e035d3886ae36ec28b0373d092d82ca392b37d3
Jeswin Kumar <jeswinpk@agilehead.com>
2018-07-18 20:53:46 +0530

    WIP

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,48 @@ import * as pool from "./pool";
 
 export { default as setup } from "./setup";
 
+export interface IFeedEntry {
+  html: string;
+}
+
+export interface IFeed {
+  items: IFeedEntry[];
+}
+
+export interface IDateLinkEntry {
+  date: string;
+  link: string;
+  count: number;
+}
+
+export interface IDateLinks {
+  items: IDateLinkEntry[];
+}
+
+export interface IPinnedEntry {
+  html: string;
+}
+
+export interface IPinned {
+  items: IPinnedEntry[];
+}
+
 export async function init(dbConfig: IDbConfig) {
   return {
     pool: await pool.init(dbConfig)
   };
+}
+
+export async function getFeed(username: string) {
+  return {
+    items: []
+  } as IFeed;
+}
+
+export async function getDateLinks(username: string) {
+  return { items: [] } as IDateLinks;
+}
+
+export async function getPinned(username: string) {
+  return { items: [] } as IPinned;
 }
